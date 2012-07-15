e64f7681800f2c3ccec40639ddac9b9113ab65cd
SurfaceS <nif@gnld.ch>
2012-07-16 01:36:00 +0300

    Update master

--- Scripts/antiAFK.lua
+++ Scripts/antiAFK.lua
@@ -4,24 +4,26 @@
         Written by Kilua
 ]]
 
-player = GetMyHero()
-tick = nil
-xtemp = player.x
-ztemp = player.z
+if myHero == nil then myHero = GetMyHero() end
+antiAFK = {}
+antiAFK.tick = nil
+antiAFK.x = myHero.x
+antiAFK.z = myHero.z
+antiAFK.timer = 0
 
-function tickHandler()
-	if tick == nil and player.x == xtemp and player.z == ztemp then
-		tick = GetTickCount()
-		timer = math.random(50000,100000)
-	elseif tick ~= nil and GetTickCount() - tick > timer and player.x == xtemp and player.z == ztemp then
-		player:MoveTo(player.x,player.z)
-		tick = nil
-	elseif player.x ~= xtemp or player.z ~= ztemp then
-		xtemp = player.x
-		ztemp = player.z
-		tick = nil
+function antiAFK.tickHandler()
+	if antiAFK.tick == nil and myHero.x == antiAFK.x and myHero.z == antiAFK.z then
+		antiAFK.tick = GetTickCount()
+		antiAFK.timer = math.random(50000,100000)
+	elseif antiAFK.tick ~= nil and GetTickCount() - antiAFK.tick > timer and myHero.x == antiAFK.x and myHero.z == antiAFK.z then
+		myHero:MoveTo(myHero.x,myHero.z)
+		antiAFK.tick = nil
+	elseif myHero.x ~= antiAFK.x or myHero.z ~= antiAFK.z then
+		antiAFK.x = myHero.x
+		antiAFK.z = myHero.z
+		antiAFK.tick = nil
 	end
 end
 
 PrintChat(" >> AntiAfk Loaded !")
-BoL:addTickHandler(tickHandler,100)
+BoL:addTickHandler(antiAFK.tickHandler,5000)
