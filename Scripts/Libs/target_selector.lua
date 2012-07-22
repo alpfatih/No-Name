f3f0d1cd294b00be24e0c1d4d6ab482a16407f6e
ikita <sitnalta@gmail.com>
2012-07-22 17:45:21 +0800

    updated

--- Scripts/Libs/target_selector.lua
+++ Scripts/Libs/target_selector.lua
@@ -1,59 +1,62 @@
 --[[
-   Library: Target Selector Library v0.3a
-    Author: Modified by Keoshin/Taikumi
-   Credits: This script was largely (99.9%) based on FPB's Target Selector Library by H0nda/Weee.
-			Credits to them for making an awesome library.	Their library was largely modified to
-			work with Bot of Legends API's.
-            delusionallogic - for helping me test ChatHandler()
+Library: Target Selector Library v0.3b
+Author: By llama, modified by Keoshin/Taikumi
+Credits: This script was largely (99.9%) based on FPB's Target Selector Library by llama/H0nda/Weee.
+Credits to them for making an awesome library. Their library was largely modified to
+work with Bot of Legends API's.
+delusionallogic - for helping me test ChatHandler()
 
-	  NOTE:	This script does not support all functionalities yet. Only the basic LowHP targetting mode
-			is supported. Others (such as MostAP, Priority Mode) will be added soon.
+NOTE: This script does not support all functionalities yet. Only the basic LowHP targetting mode
+is supported. Others (such as MostAP, Priority Mode) will be added soon.
 
- Changelog:
-       v0.1 - Initial release
-       v0.2 - Changed printarray to print out the champion name, instead of player name
-            - Added support for MostAP, Highest Priority mode selector.
-            - Added in-game chat command parsing:
-                .lowhp (Switches target selector to lowest effective hp mode)
-                .mostap (Switches target selector to most ap mode)
-                .primode (Switches target selector to highest priority mode)
-                .ignore charName (ignores charName and will not target it)
-                .focus charName (sets the charName as highest priority, reset all others to 0)
-                .setpriority charName priority# (Priority # ranges from 1: highest to 5: lowest)
-                .setpriorityarray 2 3 1 5 4 = (Champion #2 gets highest priority, #3 get's 2nd highest, so on)
+Changelog:
+v0.1 - Initial release
+v0.2 - Changed printarray to print out the champion name, instead of player name
+- Added support for MostAP, Highest Priority mode selector.
+- Added in-game chat command parsing:
+.lowhp (Switches target selector to lowest effective hp mode)
+.mostap (Switches target selector to most ap mode)
+.primode (Switches target selector to highest priority mode)
+.ignore charName (ignores charName and will not target it)
+.focus charName (sets the charName as highest priority, reset all others to 0)
+.setpriority charName priority# (Priority # ranges from 1: highest to 5: lowest)
+.setpriorityarray 2 3 1 5 4 = (Champion #2 gets highest priority, #3 get's 2nd highest, so on)
 
-            - NOTE: For in-game commands support, you must have a chatHandler in your script that passes the message to
-                    TargetSelector:get_chat_command(msg)
+- NOTE: For in-game commands support, you must have a chatHandler in your script that passes the message to
+TargetSelector:get_chat_command(msg)
 
-       v0.3 - Changed priority numbering system. 1 is now the highest priority, while 5 is lowest.
-            - Added Low HP Priority Mode:
-                .hpprimode (Switches target selector to Low HP priority mode based on priority # modifier)
-                    A champion with priority #5 will essentially have 5 * maxHP, compared with a champion with priority #1 
-                    which will have 1 * maxHP.
-	    - Target selector will now ignore targets which are not targetable, i.e. Fizz's jump, Vladmimir's pool, Zhonya's Hourglass, GA pop.
+v0.3a - Changed priority numbering system. 1 is now the highest priority, while 5 is lowest.
+- Added Low HP Priority Mode:
+.hpprimode (Switches target selector to Low HP priority mode based on priority # modifier)
+A champion with priority #5 will essentially have 5 * maxHP, compared with a champion with priority #1
+which will have 1 * maxHP.
+v0.3b - Fixed bug with .setpriorityarray not properly setting priority.
+- Credits to llama, original author of the script.
+- ts:buildarray() is now deprecated. The script will automatically build the enemy array once the target selector
+is loaded. You dno not need to call this function.
 ]]--
 
 
 do
 
 -- Library related constants
-TARGET_LOW_HP   = 1
-TARGET_MOST_AP  = 2
+TARGET_LOW_HP = 1
+TARGET_MOST_AP = 2
 TARGET_PRIORITY = 3
-DAMAGE_MAGIC    = 1
+DAMAGE_MAGIC = 1
 DAMAGE_PHYSICAL = 2
 
 
 --[[
-    Todo: ignore targets with the buffs:
-        -Vlads pool
-        -Zhonyas
-        -enemy being in Guardian Angel, lying on ground
-        -Kogmaw passive
-        -Karthus passive
-        -Alistar ult
-        -Kayle ult
-        -Tryn ult
+Todo: ignore targets with the buffs:
+-Vlads pool
+-Zhonyas
+-enemy being in Guardian Angel, lying on ground
+-Kogmaw passive
+-Karthus passive
+-Alistar ult
+-Kayle ult
+-Tryn ult
 ]]--
 
 
@@ -74,7 +77,7 @@ local targetMode = ""
 
 
 local function split(pString, pPattern)
-   local Table = {}  -- NOTE: use {n = 0} in Lua-5.0
+   local Table = {} -- NOTE: use {n = 0} in Lua-5.0
    local fpat = "(.-)" .. pPattern
    local last_end = 1
    local s, e, cap = pString:find(fpat, 1)
@@ -106,6 +109,8 @@ function TargetSelector:new(mode, range, damageType)
     end
 
     PrintChat("[TS] Target Selector loaded - Target Mode:" .. targetMode)
+    object:buildarray()
+
     return object
 end
 
@@ -116,7 +121,7 @@ function TargetSelector:buildarray()
         local obj = heroManager:getHero(i)
         if obj ~= nil and obj.team ~= player.team then
             EnemyTargets[n].enemyObj = obj
-			n = n + 1
+n = n + 1
         end
     end
 end
@@ -125,7 +130,7 @@ end
 function TargetSelector:printarray()
     for i, target in ipairs(EnemyTargets) do
         if target.enemyObj ~= nil then
-            PrintChat("[TS] Enemy " .. i .. ": " .. target.enemyObj.charName .. "    Mode=" .. (target.ignore and "ignore" or "target") .."    Priority=" .. target.priorityNum)
+            PrintChat("[TS] Enemy " .. i .. ": " .. target.enemyObj.charName .. " Mode=" .. (target.ignore and "ignore" or "target") .." Priority=" .. target.priorityNum)
         end
     end
     PrintChat("[TS] Target mode: " .. targetMode)
@@ -192,14 +197,6 @@ ChatCommands = {
         end
     },
 
-    { -- .primode
-        command = "primode", requiresTarget = false, method =
-        function(i, cmd)
-            targetMode = "Priority"
-            PrintChat("[TS] Focusing enemy with the highest priority")
-        end
-    },
-    
     { -- .hpprimode
         command = "hpprimode", requiresTarget = false, method =
         function(i, cmd)
@@ -208,11 +205,19 @@ ChatCommands = {
         end
     },
     
+    { -- .primode
+        command = "primode", requiresTarget = false, method =
+        function(i, cmd)
+            targetMode = "Priority"
+            PrintChat("[TS] Focusing enemy with the highest priority")
+        end
+    },
+        
     { -- setpriorityarray 1 2 3 4 5 -- [pass in the index of the champions at at each specified parameter, with 5 being the highest]
         command = "setpriorityarray", requiresTarget = false, method =
         function(i, cmd)
             cmd = cmd:gsub(CommandPrefix .. "setpriorityarray ", "") -- Remove the command prefix
-            args = split(cmd, " ")
+            local args = split(cmd, " ")
 
             if #args ~= #EnemyTargets then
                 PrintChat("[TS] Invalid arguments! You need 5 arguments, each signifying the champion index.")
@@ -225,13 +230,13 @@ ChatCommands = {
 
             for priority = 1, #args do
                 local index = tonumber(args[priority])
-                if index ~= nil then
-                    if EnemyTargets[index].enemyObj ~= nil then
-                        PrintChat("[TS] "..EnemyTargets[index].enemyObj.charName.."'s  priority set to  ".. priority)
+                if index >= 1 and index <= #EnemyTargets then
+                    if EnemyTargets[index].enemyObj then
+                        PrintChat("[TS] " .. EnemyTargets[index].enemyObj.charName .. "'s priority set to " .. priority)
+                        EnemyTargets[index].priorityNum = priority
                     end
-                    EnemyTargets[index].priorityNum = priority
                 else
-                    EnemyTargets[index].priorityNum = 0
+                    EnemyTargets[index].priorityNum = 1
                 end
             end
             
@@ -250,7 +255,7 @@ ChatCommands = {
 
             if n >= 1 and n <= 5 then
                 EnemyTargets[i].priorityNum = n
-                PrintChat("[TS] "..EnemyTargets[i].enemyObj.charName.."'s  priority set to  "..n)
+                PrintChat("[TS] "..EnemyTargets[i].enemyObj.charName.."'s priority set to "..n)
                 if targetMode ~= "Priority" then
                     PrintChat("[TS] Focusing enemy with the highest priority")
                 end
@@ -260,6 +265,7 @@ ChatCommands = {
             end
         end
     },
+    
     { -- .ignore champName
         command = "ignore", requiresTarget = true, method =
         function(i, cmd)
@@ -292,29 +298,29 @@ end
 
 -- Returns lowest effective HP target that is in range
 function TargetSelector:get_low_hp()
-	local selected = nil
+local selected = nil
 
-	for i, target in ipairs(EnemyTargets) do
+for i, target in ipairs(EnemyTargets) do
         local current = target.enemyObj
-		if self:valid_target(current) and player:GetDistance(current) <= self.range and not target.ignore then
+if self:valid_target(current) and player:GetDistance(current) <= self.range and not target.ignore then
 
-			if not selected then
-				selected = current
-			else
-				-- Handles damage type: DAMAGE_MAGIC and DAMAGE_PHYSICAL
-				if self.damageType == DAMAGE_MAGIC then
-					if selected.health * (100 / player:CalcMagicDamage(selected, 100)) > current.health * (100 / player:CalcMagicDamage(current, 100)) then
-						selected = current
-					end
-				elseif self.damageType == DAMAGE_PHYSICAL then
-					local player_totalDamage = player.damage + player.addDamage
-					if selected.health * (player_totalDamage / player:CalcDamage(selected)) > current.health * (player_totalDamage / player:CalcDamage(current)) then
-						selected = current
-					end
-				end
-			end
-		end
-	end
+if not selected then
+selected = current
+else
+-- Handles damage type: DAMAGE_MAGIC and DAMAGE_PHYSICAL
+if self.damageType == DAMAGE_MAGIC then
+if selected.health * (100 / player:CalcMagicDamage(selected, 100)) > current.health * (100 / player:CalcMagicDamage(current, 100)) then
+selected = current
+end
+elseif self.damageType == DAMAGE_PHYSICAL then
+local player_totalDamage = player.damage + player.addDamage
+if selected.health * (player_totalDamage / player:CalcDamage(selected)) > current.health * (player_totalDamage / player:CalcDamage(current)) then
+selected = current
+end
+end
+end
+end
+end
 
     self.target = selected
 end
@@ -344,17 +350,17 @@ end
 
 -- Returns target with highest priority # that is in range
 function TargetSelector:get_highest_priority()
-	local selected = nil
+local selected = nil
 
-	for i, target in ipairs(EnemyTargets) do
+for i, target in ipairs(EnemyTargets) do
         if self:valid_target(target.enemyObj) and player:GetDistance(target.enemyObj) <= self.range then
-			if not selected then
-				selected = target
-			elseif target.priorityNum <= selected.priorityNum then
+if not selected then
+selected = target
+elseif target.priorityNum <= selected.priorityNum then
                 selected = target
-			end
-		end
-	end
+end
+end
+end
     
     if not selected then
         self.target = nil
@@ -366,31 +372,37 @@ end
 
 -- Returns lowest effective HP target (with modifier) that is in range
 function TargetSelector:get_low_hp_priority()
-	local selected = nil
+local selected = nil
 
-	for i, target in ipairs(EnemyTargets) do
+for i, target in ipairs(EnemyTargets) do
         local current = target.enemyObj
-		if self:valid_target(current) and player:GetDistance(current) <= self.range and not target.ignore then
+if self:valid_target(current) and player:GetDistance(current) <= self.range and not target.ignore then
 
-			if not selected then
-				selected = current
-			else
-				-- Handles damage type: DAMAGE_MAGIC and DAMAGE_PHYSICAL
-				if self.damageType == DAMAGE_MAGIC then
-					if selected.health * selected.priorityNum * (100 / player:CalcMagicDamage(selected, 100)) > current.health * current.priorityNum * (100 / player:CalcMagicDamage(current, 100)) then
-						selected = current
-					end
-				elseif self.damageType == DAMAGE_PHYSICAL then
-					local player_totalDamage = player.damage + player.addDamage
-					if selected.health * selected.priorityNum * (player_totalDamage / player:CalcDamage(selected)) > current.health * current.priorityNum * (player_totalDamage / player:CalcDamage(current)) then
-						selected = current
-					end
-				end
-			end
-		end
-	end
+if not selected then
+selected = current
+else
+-- Handles damage type: DAMAGE_MAGIC and DAMAGE_PHYSICAL
+if self.damageType == DAMAGE_MAGIC then
+if selected.health * selected.priorityNum * (100 / player:CalcMagicDamage(selected, 100)) > current.health * current.priorityNum * (100 / player:CalcMagicDamage(current, 100)) then
+selected = current
+end
+elseif self.damageType == DAMAGE_PHYSICAL then
+local player_totalDamage = player.damage + player.addDamage
+if selected.health * selected.priorityNum * (player_totalDamage / player:CalcDamage(selected)) > current.health * current.priorityNum * (player_totalDamage / player:CalcDamage(current)) then
+selected = current
+end
+end
+end
+end
+end
 
     self.target = selected
+end
+
+
+-- Check if the target object is currently in a state where we should ignore.
+function TargetSelector:check_target_state(targetObj)
+    return false
 end
 
 
