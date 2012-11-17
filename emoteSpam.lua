6e6a508c21a4130253174b67a3920c39ee1a7531
ikita <sitnalta@gmail.com>
2012-11-17 22:30:45 +0800

    released

---
+++ emoteSpam.lua
@@ -0,0 +1,51 @@
+--[[
+ikita's Emote spam on spell casts/attacks
+credits to Broland and his Funny Phrases
+]]
+
+HK = string.byte("7")
+ 
+--[[ Config ]] --
+function OnLoad()
+scriptActiveS = true
+PrintChat(" >> Emote Spam loaded")
+end
+
+-- (dance = 0, taunt=1, laugh=2, joke=3)
+function Emote(id)
+	p = CLoLPacket(0x45)
+	p:EncodeF(myHero.networkID)
+	p:Encode1(id)
+	p.dwArg1 = 1
+	p.dwArg2 = 0
+	SendPacket(p)
+end
+
+
+--[[ Code ]] --
+
+function OnProcessSpell(object, spell)
+local spellName = spell.name
+	if object.name == player.name and scriptActiveS then
+	--Emote(math.floor(math.random(3))) --ENABLE this line for spamming every auto attack. (warning: cancels AA at low attack speeds)
+	--PrintChat(spellName) -- USE this line to find spell names to include in exclusions in the following line:
+		if (not (string.find(spellName,"Attack") or string.find(spellName,"Crit") or string.find(spellName,"Basic"))) then --add spell exclusions here		
+			Emote(math.floor(math.random(3)))		
+		end	
+	end
+end
+  
+ 
+function OnWndMsg( msg, keycode )
+	if msg == KEY_DOWN then 
+		if keycode == HK then
+			if scriptActiveS then
+				scriptActiveS = false
+				PrintChat(" >> Emote Spam disabled!")  
+			else
+				scriptActiveS = true
+				PrintChat(" >>  Emote Spam enabled!")
+			end     
+		end
+	end
+end
---
+++ itemSorter.lua
@@ -0,0 +1,66 @@
+--[[
+ikita's item sorter script 1.0
+]]
+
+HK = string.byte("8")
+ 
+--[[ Config ]] --
+            --Q, W, E, R, D, F, 1, 2, 3, 4, 5, 6
+keypress = {81,87,69,82,68,70,49,50,51,52,53,54}
+
+
+
+function OnLoad()
+scriptActive = false
+PrintChat(" >> ikita's Item sorter loaded")
+end
+
+--Item swap function
+function swap(slotA,slotB)
+	p = CLoLPacket(0x20)
+	p.dwArg1 = 1
+	p.dwArg2 = 0
+	p:EncodeF(myHero.networkID)
+	p:Encode1(slotA)
+	p:Encode1(slotB)
+	SendPacket(p)
+end
+
+
+--[[ Code ]] --
+
+function OnTick()
+if scriptActive then
+for i = 0, 6, 1 do
+swap(i,i+1)
+end
+end
+end
+
+
+ 
+function OnWndMsg( msg, keycode )
+	if msg == KEY_DOWN then 
+		if keycode == HK then
+			if scriptActive then
+				scriptActive = false
+				PrintChat(" >> Item sort disabled!")  
+			else
+				scriptActive = true
+				PrintChat(" >> Item sort enabled!")
+			end     
+		end
+	end
+	if msg == KEY_DOWN then
+		for i = 0, #keypress, 1 do
+			if keycode == keypress[i] then
+				scriptActive = false
+				tempStop = true
+			end
+		end
+	end
+	if msg == WM_LBUTTONUP and tempStop then
+		scriptActive = true
+		tempStop = false
+	end
+end
