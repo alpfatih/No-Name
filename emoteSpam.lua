8f6ac0fe13143aee8fac66c38e1f1cfcec7db776
ikita <sitnalta@gmail.com>
2013-03-14 04:14:00 +0800

    updated offsets

--- emoteSpam.lua
+++ emoteSpam.lua
@@ -1,41 +1,35 @@
---[[
-ikita's Emote spam on spell casts/attacks
-credits to Broland and his Funny Phrases
+--[[
+ikita's Emote spam
+credits to Broland and his Funny Phrases
 ]]
 
-HK = string.byte("7")
- 
+HK = string.byte("8")
+ 
 --[[ Config ]] --
 function OnLoad()
 scriptActiveS = true
-PrintChat(" >> Emote Spam loaded")
-end
-
--- (dance = 0, taunt=1, laugh=2, joke=3)
-function Emote(id)
-	p = CLoLPacket(0x45)
-	p:EncodeF(myHero.networkID)
-	p:Encode1(id)
-	p.dwArg1 = 1
-	p.dwArg2 = 0
-	SendPacket(p)
+PrintChat(" >> Emote Spam loaded")
 end
 
-
+-- (dance = 0, taunt=1, laugh=2, joke=3)
+function Emote(id)
+	p = CLoLPacket(0x47)
+	p:EncodeF(myHero.networkID)
+	p:Encode1(id)
+	p.dwArg1 = 1
+	p.dwArg2 = 0
+	SendPacket(p)
+end
+
+
 --[[ Code ]] --
 
-function OnProcessSpell(object, spell)
-local spellName = spell.name
-	if object.name == player.name and scriptActiveS then
-	--Emote(math.floor(math.random(3))) --ENABLE this line for spamming every auto attack. (warning: cancels AA at low attack speeds)
-	--PrintChat(spellName) -- USE this line to find spell names to include in exclusions in the following line:
-		if (not (string.find(spellName,"Attack") or string.find(spellName,"Crit") or string.find(spellName,"Basic"))) then --add spell exclusions here		
-			Emote(math.floor(math.random(3)))		
-		end	
-	end
+function OnTick()	
+PrintChat("hi")
+	Emote(math.floor(math.random(3)))		
 end
-  
- 
+  
+ 
 function OnWndMsg( msg, keycode )
 	if msg == KEY_DOWN then 
 		if keycode == HK then
